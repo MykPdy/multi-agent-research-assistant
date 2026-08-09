bd29c451eff3c032bd09297eb03611dfea97c228
Mayank <mayankpandey16122002@gmail.com>
2026-08-09 23:03:34 +0530

    fix: make unused Gemini APi key optional

--- src/config.py
+++ src/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     GROQ_API_KEY: str
-    GOOGLE_API_KEY: str
+    GOOGLE_API_KEY: str = ""
     TAVILY_API_KEY: str
 
     
